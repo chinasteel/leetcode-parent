c324c69419228998c58cfda8694351e80b506a8a
steel <gengchaogang@unisound.com>
2021-06-01 11:27:39 +0800

    :construction: add int to roman second solution class.

--- leetcode-question-bank/src/main/java/com/steel/leetcode/question/bank/IntToRoman.java
+++ leetcode-question-bank/src/main/java/com/steel/leetcode/question/bank/IntToRoman.java
@@ -67,6 +67,8 @@ public class IntToRoman {
 	String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
 	String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
 	String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+	int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+	String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
 	public static void main(String[] args) {
 		IntToRoman intToRoman = new IntToRoman();
@@ -79,4 +81,21 @@ public class IntToRoman {
 			tens[num % 100 / 10] +
 			ones[num % 10];
 	}
+
+	public String intToRoman2(int num) {
+		StringBuilder roman = new StringBuilder();
+		for (int i = 0; i < values.length; ++i) {
+			int value = values[i];
+			String symbol = symbols[i];
+			while (num >= value) {
+				num -= value;
+				roman.append(symbol);
+			}
+			if (num == 0) {
+				break;
+			}
+		}
+		return roman.toString();
+	}
+
 }
